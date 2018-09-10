01ef0ec3e2468c6bb5fa6d7ab1576edc532b0d98
Ronald Huereca <rhuereca@updraftplus.com>
2018-09-10 17:02:21 -0500

    Adding docblocks and filter query.

--- wpe-page-template-viewer.php
+++ wpe-page-template-viewer.php
@@ -22,13 +22,52 @@ class WPE_Page_Template_Viewer {
 			add_filter( 'manage_pages_columns', array( $this, 'manage_page_columns' ) );
 			add_action( 'manage_pages_custom_column', array( $this, 'manage_pages_custom_column' ) );
 			add_action( 'restrict_manage_posts' , array( $this, 'maybe_output_template_filter' ), 10, 2 );
-			$this->page_templates =  wp_get_theme()->get_page_templates();
+			add_filter( 'parse_query', array( $this, 'filter_templates' ) );
+			$this->page_templates = wp_get_theme()->get_page_templates();
 		}
 	}
 
+	/**
+	 * Modifies query to filter for template type.
+	 *
+	 * Modifies query to filter for template type.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param object $query WordPress' main query
+	 * @return object $query WordPress' modified main query if template matches
+	 */
+	public function filter_templates( $query ) {
+		if( ( ! is_admin() || ! $query->is_main_query() ) ) { 
+			return $query;
+		}
+		if( 'page' !== $query->get('post_type' ) || ! isset( $_REQUEST['page-template'] ) ) {
+			return $query;
+		};
+		if( 'none' === $_REQUEST['page-template'] ) {
+			return $query;
+		}
+
+		$query->set( 'meta_key', '_wp_page_template' );
+		$query->set( 'meta_value', sanitize_text_field( $_REQUEST['page-template'] ) );
+		return $query;
+	}
+
+	/**
+	 * Adds template filter to page.
+	 *
+	 * Adds template filter to page.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $post_type   Post type to add filter for.
+	 * @param string $which       Position of filter.
+	 * @return void
+	 */
 	public function maybe_output_template_filter( $post_type, $which ) {
 		if ( 'page' !== $post_type ) return;
 		echo '<select id="page-template" name="page-template">';
+		printf( '<option value="none">%s</option>', esc_html__( 'Select a Template', 'wpe-page-template-viewer' ) );
 		printf( '<option value="default">%s</option>', esc_html__( 'Default', 'wpe-page-template-viewer' ) );
 		foreach( $this->page_templates as $slug => $name ) {
 			printf( '<option value="%s">%s</option>', esc_attr( $slug ), esc_html( $name ) );
@@ -36,11 +75,32 @@ class WPE_Page_Template_Viewer {
 		echo '</select>';	
 	}
 
+	/**
+	 * Adds a custom column to the page post type.
+	 *
+	 * Adds a new page template column to the page post type.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $columns Associative array of page columns
+	 * @return array $columns Associative array of page columns
+	 */
 	public function manage_page_columns( $columns ) {
 		$columns['page_template'] = 'Template';
 		return $columns;
 	}
 
+	/**
+	 * Output template column value.
+	 *
+	 * Output template column value.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $column_name Name of the column
+	 * @param int    $page_id     ID of the page
+	 * @return void
+	 */
 	public function manage_pages_custom_column( $column_name, $page_id ) {
 		if( 'page_template' === $column_name ) {
 			$template = get_page_template_slug( $page_id );
